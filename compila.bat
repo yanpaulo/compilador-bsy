443feae103563c7c0013b202a3f9deea27634545
Yan Paulo <yanpaulo@hotmail.com>
2018-09-22 14:04:17 -0300

    Nome de variável

--- compila.bat
+++ compila.bat
@@ -1,8 +1,8 @@
 @echo off
 SET "MINGW_DIR=C:\MinGW\bin"
-SET "FB_DIR=C:\Users\yanpa\Downloads\win_flex_bison-2.5.15"
+SET "FLEX_BISON_DIR=C:\Users\yanpa\Downloads\win_flex_bison-2.5.15"
 
-SET PATH=%PATH%;%MINGW_DIR%;%FB_DIR%
+SET PATH=%PATH%;%MINGW_DIR%;%FLEX_BISON_DIR%
 
 win_flex analisador.l
 win_bison -d parser.y
