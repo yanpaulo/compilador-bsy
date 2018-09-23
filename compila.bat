e4455ddff7a9906d4983a30770fd13b33a63c4c8
Yan Paulo <yanpaulo@hotmail.com>
2018-09-22 22:55:27 -0300

    Compilação lib de suporte.

--- compila.bat
+++ compila.bat
@@ -6,4 +6,4 @@ SET PATH=%PATH%;%MINGW_DIR%;%FLEX_BISON_DIR%
 
 win_flex analisador.l
 win_bison -d parser.y
-gcc parser.tab.c lex.yy.c -o calcula.exe
+gcc parser.tab.c lex.yy.c suporte.c -o calcula.exe
